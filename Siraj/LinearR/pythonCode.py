34d184457670c1156dab4f413deaf8a9bdb12899
sina sima <simacna@gmail.com>
2018-01-18 16:51:22 -0500

    thurs

--- Siraj/LinearR/pythonCode.py
+++ Siraj/LinearR/pythonCode.py
@@ -1,4 +1,12 @@
+#yt link (https://www.youtube.com/watch?v=XdM6ER7zTLk&list=PL2-dafEMk2A7YdKv4XfKpfbTH5z6rEEj3&index=2)
 from numpy import *
+
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+  b = starting_b
+  m = starting_m
+
+  for i in range(num_iterations):
+    b, m = step_gradient(b, m, array(points), learning_rate) #take x, y value and feed it into array
 
 
 def run():
@@ -10,7 +18,7 @@ def run():
   initial_b = 0
   initial_m = 0 #will learn these values over time
   num_iterations = 1000 #small data set so we're doing low number of iterations, incorporate gpu's etc
-  [b, m] = gradient_descent_runner
+  [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
   print(b)
   print(m)
 
